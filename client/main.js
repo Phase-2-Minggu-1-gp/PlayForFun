730e60521b8aa9b4fdb0fab398af88705741ea17
sy-permana <suryapermana996@gmail.com>
2020-09-04 02:48:53 +0800

    done register and login - finished register for html and jquery - not done for error handling

--- client/main.js
+++ client/main.js
@@ -7,7 +7,29 @@ $( document ).ready(function() {
     $('form').submit(function(event){
         event.preventDefault();
     })
+    auth()
 });
+
+function auth() {
+    $('#login').hide()
+    $('#register').hide()
+    $('#errors').hide()
+    $('#success').hide()
+    if(localStorage.token) {
+
+    } else {
+        $('#login').show()
+    }
+}
+
+function showRegister(){
+    $('#register').show()
+    $('#login').hide()
+}
+
+function cancel(){
+    auth()
+}
 
 function login() {
     const email = $('#login-email').val();
@@ -22,25 +44,45 @@ function login() {
     })
     .done(data => {
         localStorage.setItem('token', data.token)
-
+        auth()
     })
     .fail(err => {
-        console.log(err.responseJSON)
+        $('#errors').empty().append(err.responseJSON.error).show()
     })
     .always(_=> {
         $('input').val("");
     })
 }
 
-// function register() {
-//     const email = $('#register-email').val();
-//     const password = $('#register-password').val();
-//     $.ajax({
-//         url : `${baseUrl}/users/register`,
-//         method : 'post',
-//         data : {
-//             email,
-//             password
-//         }
-//     })
-// }
+function register() {
+    const email = $('#register-email').val();
+    const password = $('#register-password').val();
+    $.ajax({
+        url : `${baseUrl}/users/register`,
+        method : 'post',
+        data : {
+            email,
+            password
+        }
+    })
+    .done(data => {
+        $('#success').empty().append(`
+            ${data.msg}<br>
+            ${data.user.email}
+        `).show().fadeOut(3000);
+        auth()
+    })
+    .fail(err => {
+        $('#errors').empty()
+        if(err.errors) {
+            err.errors.forEach(item => {
+                console.log(item.message)
+                $('#errors').append(`${item.message} `)
+            })
+        }
+        $('#errors').show()
+    })
+    .always(_=> {
+        $('input').val("");
+    })
+}

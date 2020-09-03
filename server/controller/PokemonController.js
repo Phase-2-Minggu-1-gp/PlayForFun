92ee5ca636f65298b1865070249164214e37b158
yesiaplorina <yesiaplorina@gmail.com>
2020-09-04 00:11:45 +0700

    - add filter dropdown pokemon -

--- server/controller/PokemonController.js
+++ server/controller/PokemonController.js
@@ -5,7 +5,6 @@ const pokemon = require('pokemontcgsdk')
 
 class PokemonController{
     static async showAll(req, res, next){
-        //https://api.pokemontcg.io/v1/cards
         try {
             const endpoint = `https://api.pokemontcg.io/v1/cards`
             const response = await axios.get(endpoint);
@@ -49,6 +48,102 @@ class PokemonController{
                 const {id, name, imageUrl, types, hp, attacks,weaknesses} = card
 
                 if(types == "Grass"){
+                    result.push({
+                        id,
+                        name,
+                        imageUrl,
+                        types,
+                        hp,
+                        attacks,
+                        weaknesses
+                    })
+                }
+                
+            })
+
+            res.status(200).json({ pokemon: result })
+
+
+          } catch (error) {
+            res.status(500).json({ error: error })
+          }   
+    }
+
+    static async listByTypeWater(req, res, next){
+        try {
+            const endpoint = `https://api.pokemontcg.io/v1/cards`
+            const response = await axios.get(endpoint);
+            
+            let result = []
+
+            response.data.cards.forEach(card =>{
+                const {id, name, imageUrl, types, hp, attacks,weaknesses} = card
+
+                if(types == "Water"){
+                    result.push({
+                        id,
+                        name,
+                        imageUrl,
+                        types,
+                        hp,
+                        attacks,
+                        weaknesses
+                    })
+                }
+                
+            })
+
+            res.status(200).json({ pokemon: result })
+
+
+          } catch (error) {
+            res.status(500).json({ error: error })
+          }   
+    }
+
+    static async listByTypePsychic(req, res, next){
+        try {
+            const endpoint = `https://api.pokemontcg.io/v1/cards`
+            const response = await axios.get(endpoint);
+            
+            let result = []
+
+            response.data.cards.forEach(card =>{
+                const {id, name, imageUrl, types, hp, attacks,weaknesses} = card
+
+                if(types == "Psychic"){
+                    result.push({
+                        id,
+                        name,
+                        imageUrl,
+                        types,
+                        hp,
+                        attacks,
+                        weaknesses
+                    })
+                }
+                
+            })
+
+            res.status(200).json({ pokemon: result })
+
+
+          } catch (error) {
+            res.status(500).json({ error: error })
+          }   
+    }
+    //Lightning
+    static async listByTypeLightning(req, res, next){
+        try {
+            const endpoint = `https://api.pokemontcg.io/v1/cards`
+            const response = await axios.get(endpoint);
+            
+            let result = []
+
+            response.data.cards.forEach(card =>{
+                const {id, name, imageUrl, types, hp, attacks,weaknesses} = card
+
+                if(types == "Lightning"){
                     result.push({
                         id,
                         name,
--- server/router/pokemonRouter.js
+++ server/router/pokemonRouter.js
@@ -6,5 +6,9 @@ const router = require('express').Router()
 
 router.get('/list', PokemonController.showAll)
 router.get('/list/grass', PokemonController.listByTypeGrass)
+router.get('/list/water', PokemonController.listByTypeWater)
+router.get('/list/psychic', PokemonController.listByTypePsychic)
+router.get('/list/lighting', PokemonController.listByTypeLightning)
+
 
 module.exports = router

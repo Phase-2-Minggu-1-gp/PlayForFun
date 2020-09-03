4e0a1c32c0d81b407ed764752f6e8b26a53b91a1
ajiYudhanto <aji.yudhanto@gmail.com>
2020-09-04 00:00:08 +0700

    merapikan html

--- client/mtg.js
+++ client/mtg.js
@@ -1,10 +1,27 @@
 const URL = "http://localhost:3000";
 
 $( document ).ready(function() {
+    landing()
     allCards()
 });
 
+function landing () {
+    $('#landing-page').show()
+    $('#search-card-container').hide()
+    $('#all-cards-container').hide()
+}
 
+function showSearch () {
+    $('#landing-page').hide()
+    $('#search-card-container').show()
+    $('#all-cards-container').hide()
+}
+
+function showAll () {
+    $('#landing-page').hide()
+    $('#search-card-container').hide()
+    $('#all-cards-container').show()
+}
 
 function allCards () {
     $.ajax({
@@ -19,7 +36,7 @@ function allCards () {
         // console.log(data)
         data.cards.forEach(card => {
             if (card.imageUrl) {
-                $('#all-cards-container').append(`
+                $('#all-cards').append(`
                 <div class="card" style="width: 18rem;">
                 <div class="card-body">
                 <h5 class="card-title">${card.name}</h5>
@@ -50,7 +67,17 @@ function findOne (event) {
     })
 
     .done(data => {
+        $('#one-card-container').empty()
         console.log(data)
+        $('#one-card-container').append(`
+            <div class="card" style="width: 18rem;">
+            <div class="card-body">
+            <h5 class="card-title">${data.card.name}</h5>
+            <p class="card-text">${data.card.rarity}</p>
+            <img src="${data.card.imageUrl}" class="card-img-top" alt="...">
+            </div>
+            </div>
+        `)
     })
 
     .fail(err => {

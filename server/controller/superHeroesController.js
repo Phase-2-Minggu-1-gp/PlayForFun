c24566269ae0798cae6798fc385a0276263f1b42
sy-permana <suryapermana996@gmail.com>
2020-09-04 11:15:39 +0800

    SuperHero API controller Server - changes controller for feedback response

--- server/controller/superHeroesController.js
+++ server/controller/superHeroesController.js
@@ -1,12 +1,35 @@
-const axios = require('axios')
+const axios = require('axios');
+const randomNum = require('../helpers/random');
+const { SuperHero } = require('../models')
 
 class SuperHerosController {
-    static async findByName(req, res) {
+    static async getRandom(req, res) {
         try {
-            const response = await axios.get(`https://superheroapi.com/api/${process.env.SUPERHERO_API_KEY}`);
+            const response = await axios.get(`https://superheroapi.com/api/${process.env.SUPERHERO_API_KEY}/${randomNum(731)}`);
+            res.status(200).json({ data : response.data })
         } catch (err) {
             res.status(err.response.status).json({ error : err.response.statusText })
         } 
+    }
+
+    static async getByName(req, res) {
+        try {
+            const name = req.params.name
+            const response = await axios.get(`https://superheroapi.com/api/${process.env.SUPERHERO_API_KEY}/search/${name}`);
+            res.status(200).json({ data : response.data })
+        } catch (err) {
+            res.status(err.response.status).json({ error : err.response.statusText })
+        } 
+    }
+
+    static async getNames(req, res){
+        try {
+            const data = await SuperHero.findAll()
+            if(!data || data.length === 0) throw ({ msg : 'data not found', status : 404 });
+            res.status(200).json({ data })
+        } catch (err) {
+            res.status(500).json({ error : err })
+        }
     }
 }
 
